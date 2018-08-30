98bca6e040f1d91ae7b7279fb71c86c8cabac526
Bill Nguyen <billnguyen@ydmglobal.net>
2018-08-30 20:51:23 +0700

    Adding Discovery service with Eureka server

--- license-service/src/main/docker/run.sh
+++ license-service/src/main/docker/run.sh
@@ -1,18 +1,32 @@
 #!/bin/sh
 
-echo "********************************************************"
-echo "Waiting for the configuration server to start on port $CONFIGSERVER_PORT"
-echo "********************************************************"
-while ! `nc -z config-server $CONFIGSERVER_PORT `; do sleep 3; done
-echo ">>>>>>>>>>>> Configuration Server has started"
+getPort() {
+    echo $1 | cut -d : -f 3 | xargs basename
+}
 
 echo "********************************************************"
-echo "Waiting for the database server to start on port $DATABASESERVER_PORT"
+echo "Waiting for the Discovery server to start on port $DISCOVERYSERVER_PORT"
+echo "********************************************************"
+while ! `nc -z discovery-server  $DISCOVERYSERVER_PORT`; do sleep 3; done
+echo "******* Discovery Server has started"
+
+
+echo "********************************************************"
+echo "Waiting for the Database Server to start on port $DATABASESERVER_PORT"
 echo "********************************************************"
 while ! `nc -z database $DATABASESERVER_PORT`; do sleep 3; done
-echo ">>>>>>>>>>>> Database Server has started"
+echo "******** Database Server has started "
 
 echo "********************************************************"
-echo "Starting License Server with Configuration Service :  $CONFIGSERVER_URI";
+echo "Waiting for the Configuration server to start on port $CONFIGSERVER_PORT"
 echo "********************************************************"
-java -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -jar /usr/local/license-service/@project.build.finalName@.jar
+while ! `nc -z config-server $CONFIGSERVER_PORT`; do sleep 3; done
+echo "*******  Configuration Server has started"
+
+echo "********************************************************"
+echo "Starting License Server with Configuration Service via Discovery endpoint : $DISCOVERYSERVER_URI" ON PORT: $SERVER_PORT;
+echo "********************************************************"
+java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$SERVER_PORT   \
+     -Deureka.client.serviceUrl.defaultZone=$DISCOVERYSERVER_URI             \
+     -Dspring.cloud.config.uri=$CONFIGSERVER_URI                          \
+     -Dspring.profiles.active=$PROFILE -jar /usr/local/license-service/@project.build.finalName@.jar
--- license-service/src/main/java/com/codekooking/licenses/LicenseServiceApplication.java
+++ license-service/src/main/java/com/codekooking/licenses/LicenseServiceApplication.java
@@ -2,12 +2,13 @@ package com.codekooking.licenses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@RefreshScope
-public class Application {
+@EnableDiscoveryClient
+public class LicenseServiceApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(LicenseServiceApplication.class, args);
     }
 }
